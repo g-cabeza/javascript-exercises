55c323818c01d6281814d8a114066d98183633e6
Guillermo Cabeza <guillermo.f.cabeza@gmail.com>
2026-06-19 20:12:23 -0300

    Went the hard way with a loop. Feel like an idiot after seeing the solution! But solved it by myself!

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let strLength = word.length - 1;
+    reversedWord ="";
+for (strLength; strLength >= 0; strLength--){
 
+    reversedWord+=word.at(strLength)
+}
+  return (reversedWord);
 };
+
+console.log(reverseString("Hello Motherfucker"));
 
 // Do not edit below this line
 module.exports = reverseString;
