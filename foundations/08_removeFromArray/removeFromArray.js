0e8a81f7c36fa07b6d43194c66c3a9a53b1cd86f
Guillermo Cabeza <guillermo.f.cabeza@gmail.com>
2026-06-20 15:48:21 -0300

    Done! The hard way (with google help for syntax). But took the hard road again, but more efficent.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,rem) {
+
+    return (arr.filter((arr) => arr !== rem));
+    
 };
+
+testArray = [1,2,3,4] ;
+testRemove = 3 ;
+
+console.log(removeFromArray(testArray,testRemove));
 
 // Do not edit below this line
 module.exports = removeFromArray;
