cdc3ee2f39bfba34cfd4904d4187c05e15aa603d
Guillermo Cabeza <guillermo.f.cabeza@gmail.com>
2026-06-20 16:50:01 -0300

    Excersive solved! Pretty much the StackOverflow solution

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  celciusTemp = (temp - 32) * (5 / 9) ;
+  return (parseFloat(celciusTemp.toFixed(2))) ;
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  farenheitTemp = ((temp * (5 / 9)) + 32) ;
+  return (parseFloat(farenheitTemp.toFixed(2))) ;
 };
+
+
+
+let temperature = 32 ; //enter the temperature here to calculate
+
+console.log (convertToFahrenheit (temperature)) ;
+
+console.log(convertToCelsius (temperature)) ;
 
 // Do not edit below this line
 module.exports = {
