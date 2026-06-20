8ec8a35772ee477e0be0cd4628be42059d9a7f96
Guillermo Cabeza <guillermo.f.cabeza@gmail.com>
2026-06-20 16:02:02 -0300

    Went the "for route" and solved!

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
 
+    let accumulator = 0;
+
+    for (let i = firstNumber; i <= lastNumber; i++) {
+        accumulator += i ;
+    
+    }
+
+    return (accumulator) ;
 };
+
+startNumber = 1 ;
+finishNumber = 4 ;
+
+console.log (sumAll(startNumber, finishNumber));
+
 
 // Do not edit below this line
 module.exports = sumAll;
